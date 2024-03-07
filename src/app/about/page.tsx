a3b698d109f2172997de1c88425ff940d04830a7
aka <aaronkaltman@gmail.com>
2024-03-06 19:25:34 -0500

    about page stuff

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,9 +5,9 @@ export default function AboutPage() {
   return (
     <>
       <Header />
-      <main className="flex flex-col max-w-3xl px-4 mx-auto justify-center items-center">
-        <h1 className="text-3xl text-center py-10">OUR MISSION</h1>
-        <p className="text-center">
+      <div className="flex flex-col justify-between items-center">
+        <h1 className="text-3xl py-10">OUR MISSION</h1>
+        <p className="max-w-3xl mx-auto text-center leading-7">
           Sasha and Farris bonded over their love for creating beautiful
           experiences for their friends, family, and community. As Rosewood
           Studio continues to grow, we want to share this passion with our
@@ -19,23 +19,25 @@ export default function AboutPage() {
           endeavors and provide learning experiences to youth. Come join us on
           this new adventure as we strive to bring beauty to your world.
         </p>
-        <h2 className="text-3xl text-center py-10">OUR LEADERS</h2>
-        <div className="flex flex-wrap justify-center w-full gap-20">
-          <div className="flex flex-1 flex-col gap-4 min-w-[300px]">
+      </div>
+      <div className="flex flex-col justify-between items-center">
+        <h2 className="text-3xl py-10">OUR LEADERS</h2>
+        <div className="grid grid-cols-2 gap-x-20">
+          <div className="flex flex-col justify-center items-center">
             <Image
               src="https://images.unsplash.com/photo-1594803617043-2a5a0829fcd9?q=80&w=2565&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
               alt="Sasha Fierro"
               width={200}
               height={200}
-              className="rounded-full h-[250px] w-[250px] object-cover"
+              className="rounded-full h-[250px] w-[250px] object-cover pb-3"
             />
-            <h2>Sasha Fierro</h2>
-            <p>
+            <h2 className="text-center">Sasha Fierro</h2>
+            <p className="text-center pb-10">
               Sasha Fierro Founder + Lead Designer
               <br />
               Sasha@RosewoodStudioATL.com Phone?
             </p>
-            <p>
+            {/* <p>
               The most beautiful flowers can only bloom from soil that has been
               fertilized with all the stages of life. Rosewood Studio is that
               flower for Sasha Fierro. Sasha began modeling at a young age, but
@@ -52,23 +54,23 @@ export default function AboutPage() {
               Casseroles thanks to an introduction to Betsy McKay by one of her
               dear friends. The time has come for Sasha’s garden to expand as we
               launch Rosewood Studio ATL.
-            </p>
+            </p> */}
           </div>
-          <div className="flex flex-1 flex-col gap-4 min-w-[300px]">
+          <div className="flex flex-col items-center">
             <Image
               src="https://images.unsplash.com/photo-1562174949-314f9bcf6436?q=80&w=2565&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
               alt="Sasha Fierro"
               width={200}
               height={200}
-              className="rounded-full h-[250px] w-[250px] object-cover"
+              className="rounded-full h-[250px] w-[250px] object-cover pb-3"
             />
-            <h2>Farris France</h2>
-            <p>
+            <h2 className="text-center">Farris France</h2>
+            <p className="text-center pb-10">
               Farris France Managing Partner
               <br />
               Farris@RosewoodStudioATL.com Phone?
             </p>
-            <p>
+            {/* <p>
               Farris came to Rosewood Studio with a unique background and set of
               work experiences. Farris started work at thirteen, heavily
               influenced by her step-father who grew up during the Great
@@ -78,10 +80,10 @@ export default function AboutPage() {
               as a restaurant franchisee. Farris brings a multifaceted
               background to Rosewood and has used this to build our brand and
               pursue our mission to give back to our community.
-            </p>
+            </p> */}
           </div>
         </div>
-      </main>
+      </div>
     </>
   );
 }
