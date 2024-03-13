c2b54dfbfaa8f72ec16330608bc6c709c7ad5a95
aka <aaronkaltman@gmail.com>
2024-03-12 20:36:58 -0400

    not bad

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,9 +1,54 @@
 import Header from "@/components/header";
+import { InstagramLogoIcon } from "@radix-ui/react-icons";
+import Image from "next/image";
 
 export default function ShopPage() {
   return (
     <div className="bg-[#A07A56]">
       <Header />
+      <h1 className="text-3xl py-10 text-white text-center">Shop</h1>
+      <h2 className="text-2xl font-bold text-green-800 pb-2 uppercase text-center">
+        Online Shopping Coming Soon
+      </h2>
+      <p className="text-zinc-50 text-center max-w-3xl mx-auto pt-2 font-Cardo">
+        For now, come see us in person. We are located inside of Casseroles,
+        1393 NORTH HIGHLAND AVENUE NORTHEAST, ATLANTA, GA, 30306, UNITED STATES
+      </p>
+      <h2 className="text-2xl font-bold text-green-800 pt-10 uppercase text-center">
+        Our Brand Partners
+      </h2>
+      <p className="text-zinc-50 text-center max-w-3xl mx-auto pt-2 font-Cardo">
+        We are committed to supporting local businesses and artists, especially
+        women. Please join us in supporting these amazing creatives. If you are
+        interested in partnering with us, please reach out to
+        Owner@RosewoodStudioATL.com
+      </p>
+      <div className="flex flex-wrap justify-evenly max-w-3xl mx-auto py-10">
+        <div className="flex flex-col">
+          <Image
+            src="https://rosewoodstudio.wpenginepowered.com/wp-content/uploads/2024/03/photo3-scaled.jpeg"
+            width={200}
+            height={200}
+            alt="alt"
+          />
+          <h2 className="py-4 text-zinc-50 text-xl text-center">
+            SOONSUN HOMEGOODS
+          </h2>
+          <InstagramLogoIcon className="flex justify-center items-center w-10 h-10 " />
+        </div>
+        <div>
+          <Image
+            src="https://rosewoodstudio.wpenginepowered.com/wp-content/uploads/2024/03/photo3-scaled.jpeg"
+            width={200}
+            height={200}
+            alt="alt"
+          />
+          <h2 className="pt-4 text-zinc-50 text-xl text-center py-4">
+            MARIA RUMERY DESIGN
+          </h2>
+          <InstagramLogoIcon className="w-10 h-10" />
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,33 +1,51 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { PRIMARYNAVIGATION } from "@/constants/image-gallery";
+import { usePathname } from "next/navigation";
 
 export default function Header() {
+  const pathname = usePathname();
+  const shopPath = pathname.includes("/shop");
+  const darkLogo =
+    "https://rosewoodstudio.wpenginepowered.com/wp-content/uploads/2024/03/logo_dark.png";
+  const lightLogo =
+    "https://rosewoodstudio.wpenginepowered.com/wp-content/uploads/2024/03/RoseWood_Main_FlowerandHome-emerald.png";
   return (
-    <header className="flex flex-col bg-[#FFFFF0] px-4">
-      <Link className="flex justify-center items-center" href="/">
-        <Image
-          className="object-contain"
-          src="https://rosewoodstudio.wpenginepowered.com/wp-content/uploads/2024/03/RoseWood_Main_FlowerandHome-emerald.png"
-          alt="logo"
-          width={300}
-          height={300}
-        />
-      </Link>
-      <nav className="flex flex-wrap items-center justify-center py-4">
-        <ul className="flex flex-wrap justify-center items-center space-x-8">
-          {PRIMARYNAVIGATION.map((navItem) => (
-            <li key={navItem.href}>
-              <Link href={navItem.href}>
-                <p className="text-[#AD5F43] text-xl hover:text-gray-900">
-                  {navItem.label}
-                </p>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </header>
+    <>
+      <div className="flex flex-col bg-[#FFFFF0]">
+        <header className={shopPath ? `bg-[#A07A56]` : `bg-[#FFFFF0]`}>
+          <Link className="flex justify-center items-center" href="/">
+            <Image
+              className="object-contain "
+              src={shopPath ? darkLogo : lightLogo}
+              alt="logo"
+              width={300}
+              height={300}
+            />
+          </Link>
+          <nav className="flex flex-wrap items-center justify-center py-4">
+            <ul className="flex flex-wrap justify-center items-center space-x-8">
+              {PRIMARYNAVIGATION.map((navItem) => (
+                <li key={navItem.href}>
+                  <Link href={navItem.href}>
+                    <p
+                      className={
+                        shopPath
+                          ? `text-white text-xl hover:text-gray-900`
+                          : `text-[#AD5F43] text-xl hover:text-gray-900`
+                      }
+                    >
+                      {navItem.label}
+                    </p>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </header>
+      </div>
+    </>
   );
 }
